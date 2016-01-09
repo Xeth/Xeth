d0f0fc1834dc3972c6fc5237f3b0ebb985a668ad
Victor Bucataru <ping@devktor.com>
2016-01-09 19:23:14 +0200

    address validation fix

--- src/wallet/AddressValidator.cpp
+++ src/wallet/AddressValidator.cpp
@@ -1,6 +1,7 @@
 
 #include "AddressValidator.hpp"
 
+
 namespace Xeth{
 
 
@@ -8,9 +9,9 @@ bool AddressValidator::validateAddress(const QString &addr)
 {
     std::string address = addr.toStdString();
 
-    if(address.compare(0, 2, "0x"))
+    if(!address.compare(0, 2, "0x"))
     {
-        if(address.length() < 22 || address.find_first_not_of("0123456789abcdefABCDEF", 2) != std::string::npos)
+        if(address.length() < 22 || address.find_first_not_of("0123456789abcdefABCDEF", 2, address.length()-2) != std::string::npos)
         {
             return false;
         }
