96151977ebaba96e448853202df54cec3456b67b
Victor Bucataru <ping@devktor.com>
2016-02-16 00:52:33 +0200

    balance in wei

--- src/command/wallet/GetBalanceCommand.cpp
+++ src/command/wallet/GetBalanceCommand.cpp
@@ -12,9 +12,7 @@ GetBalanceCommand::GetBalanceCommand(Ethereum::Connector::Provider &provider) :
 
 QVariant GetBalanceCommand::operator()(const char *address)
 {
-    Ethereum::Connector::BigInt amount = _wallet.getBalance(address);
-    boost::multiprecision::number<boost::multiprecision::cpp_dec_float<12> > balance(amount);
-    balance /= 1000000000000000000;
+    Ethereum::Connector::BigInt balance = _wallet.getBalance(address);
     return balance.str().c_str();
 }
 
