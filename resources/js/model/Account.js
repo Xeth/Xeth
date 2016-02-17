5cbeab08afa2447467bda71b94ba4e54a7cf7c97
Victor Bucataru <ping@devktor.com>
2016-02-18 00:23:53 +0200

    javascript model

--- resources/js/model/Account.js
+++ resources/js/model/Account.js
@@ -32,3 +32,56 @@ var Account = Backbone.Model.extend({
     }
 
 });
+
+
+var StealthAccount = Backbone.Model.extend({
+    idAttribute: "address",
+    initialize:function(){
+        this.set("balance",0);
+    },
+
+    update:function(){},
+    autoUpdate:function(){},
+    send:function(){
+        return false;
+    }
+});
+
+
+var AccountCollection = Backbone.Collection.extend({
+
+    initialize:function(){
+    
+        _(this).bindAll("add");
+    },
+
+    observe: function(){
+        event.Account.connect(this, this.add);
+    },
+
+    fetch:function(){
+        var accounts = wallet.getAccounts();
+        this.reset(accounts);
+    },
+
+
+    modelId: function(attrs){
+        return attrs.address;
+    },
+
+    model: function(attrs, options) {
+        var account;
+        if(attrs.address==undefined){
+            account = new StealthAccount(attrs, options);
+        }
+        else{
+            account = new Account(attrs, options);
+        }
+        account.autoUpdate();
+        return account;
+    },
+
+
+    sync:function(){}
+
+});
--- resources/js/model/AddressBook.js
+++ resources/js/model/AddressBook.js
@@ -1,3 +1,25 @@
+var Contact = Backbone.Model.extend({
+
+    idAttribute: "alias",
+
+    save:function(){
+        if(this.isNew()){
+
+            return addressbook.addContact(this.toJSON());
+        }else{
+
+            return addressbook.editContact(this.toJSON());
+        }
+    },
+
+    destroy:function(){
+        return addressbook.removeContact(this.get("alias"));
+    },
+
+    sync:function(){}
+
+});
+
 var AddressBook = Backbone.Collection.extend({
 
     model: Contact,
