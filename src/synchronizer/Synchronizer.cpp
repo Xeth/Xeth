e1e0185d7276b821d5ca03149168c1af21d3cd38
Victor Bucataru <ping@devktor.com>
2016-02-18 14:42:11 +0200

    sync interval

--- src/synchronizer/Synchronizer.cpp
+++ src/synchronizer/Synchronizer.cpp
@@ -80,7 +80,7 @@ const ChainScanner & Synchronizer::getScanner() const
 void Synchronizer::synchronize()
 {
     qDebug()<<"synchronizing...";
-    size_t scanInterval = 12000, syncInterval = 12000;
+    size_t scanInterval = 24000, syncInterval = 12000;
     _syncProgress.autoUpdate(syncInterval);
     _scanner.autoScan(scanInterval);
 }
