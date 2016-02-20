939b8d58cdbf93eddbf931883574b8fc6b8ff3f1
Victor Bucataru <ping@devktor.com>
2016-02-20 10:28:34 +0200

    transaction resolving

--- resources/js/model/AddressBook.js
+++ resources/js/model/AddressBook.js
@@ -2,6 +2,23 @@ var Contact = Backbone.Model.extend({
 
     idAttribute: "alias",
 
+    initialize:function(data, options){
+        if(options.transactions){
+            _(this).bindAll("resolve");
+            var total = 0;
+            var address = this.get("address");
+            var bitprofile = this.get("bitprofile");
+            var contact = this;
+            options.transactions.each(function(transaction){
+                if(contact._resolve(address, bitprofile, transaction)){
+                    total++;
+                }
+            });
+            this.set("transactions", total);
+            this.listenTo(options.transactions, "add", this.resolve);
+        }
+    },
+
     save:function(){
         if(this.isNew()){
 
@@ -16,13 +33,31 @@ var Contact = Backbone.Model.extend({
         return addressbook.removeContact(this.get("alias"));
     },
 
+    resolve:function(transaction){
+        var address = this.get("address");
+        var bitprofile = this.get("bitprofile");
+        if(this._resolve(this.get("address"), this.get("bitprofile"), transaction)){
+            this.set("transactions", (this.get("transactions")||0)+1);
+        }
+    },
+
+    _resolve:function(address, bitprofile, transaction){
+        if(address==transaction.get("from")||address==transaction.get("to")||(bitprofile &&bitprofile==transaction.get("bitprofile"))){
+            transaction.set("contact", this);
+            return true;
+        }
+        return false;
+    },
+
     sync:function(){}
 
 });
 
 var AddressBook = Backbone.Collection.extend({
 
-    model: Contact,
+    initialize:function(models, options){
+        this.transactions = options?options.transactions:null;
+    },
 
     fetch:function(){
         var contacts = addressbook.listContacts();
@@ -52,5 +87,8 @@ var AddressBook = Backbone.Collection.extend({
         event.Contact.connect(this, Backbone.Collection.prototype.add);
     },
 
+    model: function(data, options){
+        return new Contact(data, {transactions:options.collection.transactions});
+    }
 
 });
--- resources/template/transaction_item.tpl
+++ resources/template/transaction_item.tpl
@@ -16,18 +16,18 @@
     </div>
 </div>
 <%}else{%>
-<div class="listItem <%=transaction.type%>" <%if(transaction.alias){%>contact="true"<%}%> style="display: block;">
+<div class="listItem <%=transaction.type%>" <%if(transaction.contact){%>contact="true"<%}%> style="display: block;">
     <div class="header icon">
         <span class="detail s_titled userAddress"><%=(transaction.type=="received")?transaction.to:transaction.from%></span>
     </div>
     <span class="avatar">
-        <span class="img"><img src="<%=(transaction.alias && transaction.alias.avatar)?transaction.alias.avatar:'img/avatarEmpty.png'%>" onerror="errorAvatar($(this))"></span>
+        <span class="img"><img src="<%=(transaction.contact && transaction.contact.avatar)?transaction.contact.avatar:'img/avatarEmpty.png'%>" onerror="errorAvatar($(this))"></span>
     </span>
     <div class="data">
         <div class="detailsRow">
             <span class="userProfile">
-                <span class="name editableTxt" tabindex="-1" title=""><%=((transaction.alias && transaction.alias.name)?transaction.alias.name:"unnamed")%></span>
-                <span class="bitprofileIcon" title="<%=(transaction.alias && transaction.alias.bitprofile)?'on':'no'%> bitprofile"></span>
+                <span class="name editableTxt" tabindex="-1" title=""><%=((transaction.contact && transaction.contact.alias)?transaction.contact.alias:"unnamed")%></span>
+                <span class="bitprofileIcon" title="<%=(transaction.contact && transaction.contact.bitprofile)?'on':'no'%> bitprofile"></span>
             </span>
             <span class="detail s_titled hash" title="<%=transaction.hash%>"><%=shortify(transaction.hash, 20)%></span>
             <span class="detail txtBalance">
