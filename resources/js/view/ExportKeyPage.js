046954c71a8248d40bce55351d102b05bb389e19
Victor Bucataru <ping@devktor.com>
2016-02-20 18:05:23 +0200

    export page view

---
+++ resources/js/view/ExportKeyPage.js
@@ -0,0 +1,31 @@
+var ExportKeyPageView = Backbone.View.extend({
+
+    initialize:function(options){
+        _(this).bindAll("submit");
+        this.filesystem = options.filesystem;
+        this.accounts = options.accounts;
+        console.log("template : ", $("#export_tpl").html());
+        this.template = _.template($("#export_tpl").html());
+        this.$el.html(this.template());
+        this.$el.find(".btnSubmit").click(this.submit);
+    },
+
+    render:function(){
+        this.accounts.filter(function(model){ return !!model;});
+        this.accounts.resize();
+        this.accounts.attach(this.$el.find("#exportAddress_account"));
+    },
+
+    submit:function(){
+        var path = this.filesystem.browse();
+        var model = this.accounts.selected();
+        if(!model.backup(path)){
+            notifyError("failed to backup, please check disk space");
+            return false;
+        }
+        notifySuccess("key exported");
+        this.trigger("done");
+        return true;
+    }
+
+});
---
+++ resources/template/export.tpl
@@ -0,0 +1,9 @@
+<div class="formpage">
+    <h1>Backup address:</h1>
+    <div id="exportAddress_account"></div>
+    <div class="section_submit">
+        <div class="submit">
+            <a href="javascript:void(0);" id="exportAddress_submit" class="btnSubmit"><span class="txt">Backup</span></a>
+        </div>
+    </div>
+</div>
