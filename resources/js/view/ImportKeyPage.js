da2c5b70d36e5e71a00a128682ba6045d7eacb33
Victor Bucataru <ping@devktor.com>
2016-02-20 16:16:41 +0200

    import key page

---
+++ resources/js/view/ImportKeyPage.js
@@ -0,0 +1,37 @@
+var ImportKeyPageView = Backbone.View.extend({
+
+    initialize:function(options){
+        _(this).bindAll("render", "submit");
+        this.template = _.template($("#import_tpl").html());
+        this.$el.html(this.template());
+        this.filesystem = options.filesystem;
+        this.wallet = options.wallet;
+        this.$el.find(".browse a").click(this.render);
+        this.$el.find(".btnSubmit").click(this.submit);
+        this.fileInput = this.$el.find("#importAddress_address");
+    },
+
+    render:function(){
+        this.filename = this.filesystem.browse({type:"open"});
+        this.fileInput.val(this.filename||"");
+    },
+
+    submit:function(){
+        if(!this.filename){
+            notifyError("please select a file");
+            return false;
+        }
+        var password = this.$el.find("#importAddress_password");
+        if(!password.validate()){
+            notifyError("password is required");
+            return false;
+        }
+        if(!this.wallet.importKey({password: password, file:this.filename})){
+            notifyError("failed to import key, file is corrupted or invalid password");
+            return false;
+        }
+        notifySuccess("key imported");
+        return true;
+    }
+
+});
---
+++ resources/template/import.tpl
@@ -0,0 +1,15 @@
+<div class="formpage">
+    <h1>Import address:<span class="controls browse"><a href="javascript:void(0)">Change source file</a></span></h1>
+    <input id="importAddress_address" disabled type="text" />
+    <div class="section_submit">
+        <div class="half_left section_password">
+            <h1 error="incorrect">Password:</h1>
+            <div class="input">
+                <input id="importAddress_password" class="mandatory" type="password" />
+            </div>
+        </div>
+        <div class="submit half_right">
+            <a href="javascript:void(0);" id="importAddress_submit" class="btnSubmit"><span class="txt">Import</span></a>
+        </div>
+    </div>
+</div>
