df2159ea9d8fa4f2be50b8ae3c6678ccad423772
Victor Bucataru <ping@devktor.com>
2016-02-21 11:25:58 +0200

    set cursor method

--- resources/js/view/MenuView.js
+++ resources/js/view/MenuView.js
@@ -4,12 +4,12 @@ var MenuView = Backbone.View.extend({
         var view = this;
         this.$el.find("a").click(function(){
             var $a = $(this);
-            $a.parent().parent().find(".active").removeClass("active");
-            $a.parent().addClass("active");
             view.trigger("change", $a.attr("rel")||$a.html());
         });
     },
-    select:function(index){
-        this.$el.find("a:eq("+index+")").click();
+
+    setCursor:function(name){
+        this.$el.find(".active").removeClass("active");
+        this.$el.find("."+name).addClass("active");
     }
 });
