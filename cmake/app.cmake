ff399b850048f4eebda632536a8ddd02dbf4c33d
Victor Bucataru <ping@devktor.com>
2016-03-01 19:31:34 +0200

    templates compilation target

--- cmake/app.cmake
+++ cmake/app.cmake
@@ -25,22 +25,20 @@ include_directories(
 
 file(GLOB RESOURCE_FILES "resources/*")
 file(GLOB TEMPLATE_DIR "resources/template")
+file(GLOB TEMPLATE_QRC "resources/template.qrc")
+
 list(REMOVE_ITEM RESOURCE_FILES ${TEMPLATE_DIR})
 
 file(COPY ${RESOURCE_FILES} DESTINATION ${PROJECT_BINARY_DIR}/resources)
 file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/resources/template)
 
-add_custom_command(TARGET compiler POST_BUILD COMMAND compiler ${PROJECT_SOURCE_DIR}/resources/template ${PROJECT_BINARY_DIR}/resources/template)
+#add_custom_command(TARGET compiler POST_BUILD COMMAND compiler ${PROJECT_SOURCE_DIR}/resources/template ${PROJECT_BINARY_DIR}/resources/template)
+add_custom_target(xeth_compiled_templates COMMAND compiler ${PROJECT_SOURCE_DIR}/resources/template ${PROJECT_BINARY_DIR}/resources/template DEPENDS compiler)
+
+add_executable(xeth ${APP_SOURCES} )
+add_dependencies(xeth xeth_compiled_templates)
 
 
-add_executable(xeth ${APP_SOURCES} 
-    ${PROJECT_BINARY_DIR}/resources/icon.qrc 
-    ${PROJECT_BINARY_DIR}/resources/template.qrc 
-    ${PROJECT_BINARY_DIR}/resources/css.qrc 
-    ${PROJECT_BINARY_DIR}/resources/js.qrc 
-    ${PROJECT_BINARY_DIR}/resources/html.qrc)
-
-add_dependencies(xeth compiler)
 
 target_link_libraries(xeth
     xethlib
