f69fdfba0e26e1a04cecbfe2b8284491db9e1313
Victor Bucataru <ping@devktor.com>
2016-03-02 03:54:07 +0200

    waiting for generated templates

--- cmake/app.cmake
+++ cmake/app.cmake
@@ -28,15 +28,33 @@ file(GLOB TEMPLATE_DIR "resources/template")
 file(GLOB TEMPLATE_QRC "resources/template.qrc")
 
 list(REMOVE_ITEM RESOURCE_FILES ${TEMPLATE_DIR})
+list(REMOVE_ITEM RESOURCE_FILES ${TEMPLATE_QRC})
 
 file(COPY ${RESOURCE_FILES} DESTINATION ${PROJECT_BINARY_DIR}/resources)
 file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/resources/template)
 
-#add_custom_command(TARGET compiler POST_BUILD COMMAND compiler ${PROJECT_SOURCE_DIR}/resources/template ${PROJECT_BINARY_DIR}/resources/template)
-add_custom_target(xeth_compiled_templates COMMAND compiler ${PROJECT_SOURCE_DIR}/resources/template ${PROJECT_BINARY_DIR}/resources/template DEPENDS compiler)
 
-add_executable(xeth ${APP_SOURCES} ${PROJECT_SOURCE_DIR}/resources/template.qrc ${PROJECT_SOURCE_DIR}/resources/css.qrc ${PROJECT_SOURCE_DIR}/resources/js.qrc ${PROJECT_SOURCE_DIR}/resources/html.qrc)
-add_dependencies(xeth xeth_compiled_templates)
+
+file(GLOB TEMPLATE_FILES "${PROJECT_SOURCE_DIR}/resources/template/*.tpl")
+foreach(TEMPLATE_FILE ${TEMPLATE_FILES})
+    get_filename_component(TEMPLATE_NAME ${TEMPLATE_FILE} NAME_WE)
+    set(COMPILED_TEMPALTE_NAME ${PROJECT_BINARY_DIR}/template/${TEMPLATE_NAME}.tpl)
+    list(APPEND GENERATED_TEMPALTES ${COMPILED_TEMPALTE_NAME})
+endforeach()
+
+set(COMPILED_TEMPALTES_CPP ${PROJECT_BINARY_DIR}/template.cxx)
+
+set_source_files_properties(${GENERATED_TEMPALTES} PROPERTIES GENERATED TRUE)
+set_source_files_properties(${COMPILED_TEMPALTES_CPP} PROPERTIES GENERATED TRUE)
+
+
+add_custom_target(xeth_compiled_templates COMMAND compiler ${PROJECT_SOURCE_DIR}/resources/template ${PROJECT_BINARY_DIR}/resources/template DEPENDS compiler)
+add_custom_target(xeth_templates_qrc COMMAND  ${CMAKE_COMMAND} -E copy ${TEMPLATE_QRC} ${CMAKE_BINARY_DIR}/resources DEPENDS xeth_compiled_templates)
+add_custom_target(xeth_templates  COMMAND ${Qt5Core_RCC_EXECUTABLE} ${rcc_options} -name template -o ${COMPILED_TEMPALTES_CPP} ${PROJECT_BINARY_DIR}/resources/template.qrc DEPENDS xeth_templates_qrc)
+
+
+add_executable(xeth ${APP_SOURCES} ${COMPILED_TEMPALTES_CPP} ${PROJECT_BINARY_DIR}/resources/css.qrc ${PROJECT_BINARY_DIR}/resources/js.qrc ${PROJECT_BINARY_DIR}/resources/html.qrc)
+add_dependencies(xeth xeth_templates)
 
 
 
