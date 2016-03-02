8b8c92e6afb0b22cd31d88d579f784777ca042f0
Victor Bucataru <ping@devktor.com>
2016-03-02 21:36:54 +0200

    main page template

--- resources/js/view/MainWindow.js
+++ resources/js/view/MainWindow.js
@@ -11,82 +11,15 @@ var MainWindowView = Backbone.View.extend({
 
     initialize:function(options){
         _(this).bindAll("open");
+        this.models = {};
+        this.models.addressbook = options.addressbook;
+        this.models.accounts = options.accounts;
+        this.models.clipboard = options.clipboard;
+        this.models.filesystem = options.filesystem;
+        this.models.transactions = options.transactions;
+        this.models.progress = options.progress;
+
         this.templates = options.templates;
-        this.accounts = new AccountSelect({collection:options.accounts, templates:this.templates});
-        this.router = new PageRouter(this);
-        this.menuAlias = {default: "receive"};
-        this.subpages = {};
-        this.subpages.send = new SendPageView
-        ({
-            accounts: this.accounts, 
-            addressbook:options.addressbook, 
-            el:this.$el.find("#page_send"), 
-            router:this.router, 
-            clipboard: options.clipboard,
-            templates:this.templates
-        });
-        this.subpages.receive = new ReceivePageView
-        ({
-            accounts:this.accounts, 
-            clipboard: options.clipboard, 
-            el:this.$el.find("#page_receive"), 
-            templates:this.templates,
-            router:this.router
-        });
-        this.subpages.addressbook = new AddressBookPageView
-        ({
-            addressbook:options.addressbook,
-            el:this.$el.find("#page_addressbook"),
-            router:this.router,
-            templates:this.templates
-        });
-        this.subpages.transactions = new TransactionsPageView
-        ({
-            accounts:this.accounts,
-            transactions:options.transactions,
-            el:this.$el.find("#page_transactions"),
-            templates:this.templates
-        });
-        this.subpages.password = new ChangePasswordPageView
-        ({
-            accounts:this.accounts,
-            el:this.$el.find("#page_editAddress"),
-            router:this.router,
-            templates:this.templates
-        });
-        this.subpages.generate = new GeneratePageView
-        ({
-            accounts:options.accounts,
-            el:this.$el.find("#page_newAddress"),
-            router:this.router,
-            templates:this.templates
-        });
-        this.subpages.import = new ImportKeyPageView
-        ({
-            filesystem:options.filesystem,
-            wallet:options.accounts,
-            el:this.$el.find("#page_importAddress"),
-            router:this.router, 
-            templates:this.templates
-        });
-        this.subpages.export = new ExportKeyPageView
-        ({
-            filesystem:options.filesystem,
-            accounts:this.accounts,
-            el:this.$el.find("#page_exportAddress"),
-            router:this.router,
-            templates:this.templates
-        });
-        this.subpages.settings = new SettingsPageView
-        ({
-            el:this.$el.find("#page_settings"),
-            router:this.router,
-            templates:this.templates
-        });
-        this.subpages["default"] = this.subpages.receive;
-        this.menu = new MenuView({el:this.$el.find(".mainNav")});
-        this.menu.on("change", this.open);
-        this.progress = new ProgressView({el:this.$el.find(".footer"), model:options.progress});
     },
 
     open: function(name, args){
@@ -103,9 +36,95 @@ var MainWindowView = Backbone.View.extend({
         }
     },
 
+    bind:function(object){
+        for(var i in object){
+            if(i=="templates") this.templates = object[i];
+            else this.models[i] = object[i];
+        }
+    },
+
     render: function(){
+
         this.$el.find("#page_splash").removeClass("active").addClass("off");
+        this.$el.html(this.templates.get("main_page")());
         this.$el.addClass("loaded");
+
+        this.accounts = new AccountSelect({collection:this.models.accounts, templates:this.templates});
+        this.router = new PageRouter(this);
+        this.menuAlias = {default: "receive"};
+        this.subpages = {};
+        this.subpages.send = new SendPageView
+        ({
+            accounts: this.accounts, 
+            addressbook:this.models.addressbook, 
+            el:this.$el.find("#page_send"), 
+            router:this.router, 
+            clipboard: this.models.clipboard,
+            templates:this.templates
+        });
+        this.subpages.receive = new ReceivePageView
+        ({
+            accounts:this.accounts, 
+            clipboard: this.models.clipboard, 
+            el:this.$el.find("#page_receive"), 
+            templates:this.templates,
+            router:this.router
+        });
+        this.subpages.addressbook = new AddressBookPageView
+        ({
+            addressbook:this.models.addressbook,
+            el:this.$el.find("#page_addressbook"),
+            router:this.router,
+            templates:this.templates
+        });
+        this.subpages.transactions = new TransactionsPageView
+        ({
+            accounts:this.accounts,
+            transactions:this.models.transactions,
+            el:this.$el.find("#page_transactions"),
+            templates:this.templates
+        });
+        this.subpages.password = new ChangePasswordPageView
+        ({
+            accounts:this.accounts,
+            el:this.$el.find("#page_editAddress"),
+            router:this.router,
+            templates:this.templates
+        });
+        this.subpages.generate = new GeneratePageView
+        ({
+            accounts:this.models.accounts,
+            el:this.$el.find("#page_newAddress"),
+            router:this.router,
+            templates:this.templates
+        });
+        this.subpages.import = new ImportKeyPageView
+        ({
+            filesystem:this.models.filesystem,
+            wallet:this.models.accounts,
+            el:this.$el.find("#page_importAddress"),
+            router:this.router, 
+            templates:this.templates
+        });
+        this.subpages.export = new ExportKeyPageView
+        ({
+            filesystem:this.models.filesystem,
+            accounts:this.accounts,
+            el:this.$el.find("#page_exportAddress"),
+            router:this.router,
+            templates:this.templates
+        });
+        this.subpages.settings = new SettingsPageView
+        ({
+            el:this.$el.find("#page_settings"),
+            router:this.router,
+            templates:this.templates
+        });
+        this.subpages["default"] = this.subpages.receive;
+        this.menu = new MenuView({el:this.$el.find(".mainNav")});
+        this.menu.on("change", this.open);
+        this.progress = new ProgressView({el:this.$el.find(".footer"), model:this.models.progress});
+
         this.progress.render();
         setTimeout(this.open, 800);
     }
---
+++ resources/template/main_page.tpl
@@ -0,0 +1,40 @@
+<div id="page_send" class="page off"></div>
+<div id="page_receive" class="page off"></div>
+<div id="page_addressbook" class="page off"></div>
+<div id="page_transactions" class="page off"></div>
+<div id="page_newAddress" class="page off"></div>
+<div id="page_editAddress" class="page off"></div>
+<div id="page_importAddress" class="page off"></div>
+<div id="page_exportAddress" class="page off"></div>
+<div id="page_settings" class="page off"></div>
+<div id="page_bitprofile" class="page navpage off"></div>
+
+
+<div class="mainNav menu">
+    <a class="profile myProfile" href="javascript:void(0)" page="page_bitprofile">BitProfile</a>
+    <ul class="nav">
+        <li class="send active"><a page="page_send">send</a></li>
+        <li class="receive"><a page="page_receive">receive</a></li>
+        <li class="addressbook"><a page="page_addressbook">addressbook</a></li>
+        <li class="transactions"><a page="page_transactions">transactions</a></li>
+        <li class="settings"><a page="page_settings">settings</a></li>
+    </ul>
+    <div class="footer">
+        <div class="preloader status" loader="50">
+            <div class="message">
+                Loading...
+            </div>
+            <div class="loader">
+                <div class="value" style="width:01%;"></div>
+                <span class="txt"></span>
+            </div>
+        </div>
+        <div class="icons">
+            <div class="icon secure ok">&nbsp;</div>
+            <div class="icon network ok">&nbsp;</div>
+            <div class="icon updated">&nbsp;</div>
+        </div>
+    </div>
+</div>
+
+<div class="subnavBg"></div>
