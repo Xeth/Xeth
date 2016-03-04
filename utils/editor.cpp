919567fbb2b8ac49effd86346b89a7578440f0dd
Victor Bucataru <ping@devktor.com>
2016-03-04 19:03:30 +0200

    editor help

--- utils/editor.cpp
+++ utils/editor.cpp
@@ -9,25 +9,47 @@
 #include "lib/HtmlMerger.hpp"
 #include "lib/HtmlExtractor.hpp"
 
+void printHelp()
+{
+    std::cerr<<"invalid arguments, usage: "<<std::endl;
+    std::cerr<<"-m htmlfile templatesdir [outfile] :";
+    std::cerr<<std::endl;
+    std::cerr<<"       merges templates from (templatesdir) with (htmlfile) and saves result in (outfile), if no (outfile) is specified saves in original htmlfile";
+    std::cerr<<std::endl<<std::endl;
+    std::cerr<<"-e htmlfile templatesdest [outfile]: ";
+    std::cerr<<std::endl;
+    std::cerr<<"       extracts templates from (htmlfile) and saves them in (templatesdest) directory, stripped html is saved in (outfile) or in original htmlfile";
+    std::cerr<<std::endl<<std::flush;
+}
+
 int main(int argc, char **argv)
 {
 
     QApplication app(argc, argv);
     QCommandLineParser parser;
 
-    QCommandLineOption unpackOpt("e", QCoreApplication::translate("main", "unpack templates"));
-    QCommandLineOption packOpt("m", QCoreApplication::translate("main", "pack templates"));
+    QCommandLineOption unpackOpt("e", QCoreApplication::translate("main", "extract templates"));
+    QCommandLineOption packOpt("m", QCoreApplication::translate("main", "merge templates"));
+    QCommandLineOption helpOpt("h", QCoreApplication::translate("main", "help"));
 
     parser.addOption(unpackOpt);
     parser.addOption(packOpt);
+    parser.addOption(helpOpt);
 
     parser.process(app);
 
     QStringList args = parser.positionalArguments();
 
+    if(parser.isSet(helpOpt))
+    {
+        printHelp();
+        return 0;
+    }
+
+
     if(args.size()<2||(!parser.isSet(packOpt)&&!parser.isSet(unpackOpt)))
     {
-        std::cerr<<"invalid arguments, usage: [-u|-p] htmlfile templatesdir [outfile]"<<std::endl<<std::flush;
+        printHelp();
         return 1;
     }
 
