3c0a5fa6265e9ccbbed27829c11c65dff65b12cb
unvoo <vo@unvoo.com>
2016-03-21 12:36:31 +0200

    settings system tray

--- resources/js/view/SettingsPage.js
+++ resources/js/view/SettingsPage.js
@@ -6,6 +6,12 @@ var SettingsPageView = SubPageView.extend({
         this.$el.html(this.template());
         this.menu = new MenuView({el: this.$el.find(".btns")});
         this.menu.on("change", options.router.redirect);
+        this.trayToggle = this.$el.find("#trayToggle");
+        this.trayMinimize = this.$el.find("#trayMinimize");
+        this.trayClose = this.$el.find("#trayClose");
+        this.trayToggle.button({text:false});
+        this.trayMinimize.button({text:false});
+        this.trayClose.button({text:false});
     },
 
     render:function(){
