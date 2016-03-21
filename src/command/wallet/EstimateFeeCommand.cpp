d2805a973bbaed75a21236f5b5a8b255688c718a
Victor Bucataru <ping@devktor.com>
2016-03-21 17:34:43 +0200

    fixed estimation error nofication when using invalid address

--- src/command/wallet/EstimateFeeCommand.cpp
+++ src/command/wallet/EstimateFeeCommand.cpp
@@ -10,36 +10,42 @@ EstimateFeeCommand::EstimateFeeCommand(Ethereum::Connector::Provider &provider) 
 
 QVariant EstimateFeeCommand::operator()(const QVariantMap &request)
 {
-    if(!request.contains("from")||!request.contains("to")||!request.contains("amount"))
+    try
     {
-        return QVariant::fromValue(false);
-    }
-    QString from = request["from"].toString();
-    QString to = request["to"].toString();
-    BigInt amount = BigInt(request["amount"].toString().toStdString());
-    BigInt price = _estimator.getPrice();
-    if(request.contains("factor"))
-    {
-        size_t factor = request["factor"].toInt();
-        if(factor==0)
+        if(!request.contains("from")||!request.contains("to")||!request.contains("amount"))
         {
-            price = 0;
+            return QVariant::fromValue(false);
         }
-        else
+        QString from = request["from"].toString();
+        QString to = request["to"].toString();
+        BigInt amount = BigInt(request["amount"].toString().toStdString());
+        BigInt price = _estimator.getPrice();
+        if(request.contains("factor"))
         {
-            price *= factor;
-            price /= 100;
+            size_t factor = request["factor"].toInt();
+            if(factor==0)
+            {
+                price = 0;
+            }
+            else
+            {
+                price *= factor;
+                price /= 100;
+            }
         }
+
+        QVariantMap result;
+
+        BigInt fee = (to.length()<42) ? estimateGas(from, to, amount) : estimateStealthGas(from, to, amount);
+        result["gas"] = fee.str().c_str();
+        fee *= price;
+        result["fee"] = fee.str().c_str();
+        result["price"] = price.str().c_str();
+        return result;
     }
-
-    QVariantMap result;
-
-    BigInt fee = (to.length()<42) ? estimateGas(from, to, amount) : estimateStealthGas(from, to, amount);
-    result["gas"] = fee.str().c_str();
-    fee *= price;
-    result["fee"] = fee.str().c_str();
-    result["price"] = price.str().c_str();
-    return result;
+    catch(...)
+    {}
+    return QVariant::fromValue(false);
 }
 
 
