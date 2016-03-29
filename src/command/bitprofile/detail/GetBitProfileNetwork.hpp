7cc8c7276cbfd77733d36523d11abeeaa58d4b86
Victor Bucataru <ping@devktor.com>
2016-03-30 02:27:24 +0300

    bitprofile lib update

--- src/command/bitprofile/detail/GetBitProfileNetwork.hpp
+++ src/command/bitprofile/detail/GetBitProfileNetwork.hpp
@@ -1,15 +1,15 @@
 #pragma once
 
-#include "bitprofile/MasterRegistrar.hpp"
+#include "bitprofile/Network.hpp"
 #include "env/Settings.hpp"
 
 
 namespace Xeth{
 
 
-inline BitProfile::MasterRegistrar::Network GetBitprofileNetwork(const Settings &settings)
+inline BitProfile::Network GetBitprofileNetwork(const Settings &settings)
 {
-    return settings.get<bool>("testnet", false)?BitProfile::MasterRegistrar::Test_Net:BitProfile::MasterRegistrar::Main_Net;
+    return settings.get<bool>("testnet", false)?BitProfile::Test_Net:BitProfile::Main_Net;
 }
 
 
