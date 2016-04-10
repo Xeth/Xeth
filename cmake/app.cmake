83a17c7e6e01145f1ce0f3b8624c2ab535a42602
Victor Bucataru <ping@devktor.com>
2016-04-10 20:00:37 +0300

    using static boost

--- cmake/app.cmake
+++ cmake/app.cmake
@@ -1,6 +1,6 @@
 find_package(Qt5 COMPONENTS Core Widgets WebKit WebKitWidgets Xml Concurrent REQUIRED)
 find_package(JsonCPP REQUIRED)
-#set(Boost_USE_STATIC_LIBS ON)
+set(Boost_USE_STATIC_LIBS ON)
 find_package(Boost COMPONENTS system filesystem thread program_options random regex date_time chrono REQUIRED)
 find_package(LevelDB REQUIRED)
 find_package(GMP)
