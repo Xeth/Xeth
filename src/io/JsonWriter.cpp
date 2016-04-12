5df675cae870dac2b28c8cc51e6bc53ec87c09f2
Victor Bucataru <ping@devktor.com>
2016-04-12 20:10:35 +0300

    throw on error

--- src/io/JsonWriter.cpp
+++ src/io/JsonWriter.cpp
@@ -1,17 +1,20 @@
 #include "JsonWriter.hpp"
 
+
 namespace Xeth{
 
 JsonWriter::JsonWriter(const char *path, bool truncate) :
     _stream(path, truncate ? std::ofstream::trunc : std::ofstream::app)
-{}
+{
+    _stream.exceptions(std::ios::failbit); 
+}
 
 
 bool JsonWriter::write(const Json::Value &data)
 {
     _stream<<_parser.write(data);
     _stream.close();
-    return (bool)_stream;
+    return true;
 }
 
 
