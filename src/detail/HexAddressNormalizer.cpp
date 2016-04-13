c7dc230c242f12f6a253c0380313e97d6ae782a7
Victor Bucataru <ping@devktor.com>
2016-04-13 22:11:04 +0300

    hex address normalizer

--- src/detail/HexAddressNormalizer.cpp
+++ src/detail/HexAddressNormalizer.cpp
@@ -11,14 +11,27 @@ std::string HexAddressNormalizer::operator()(const std::string &address) const
 
 std::string HexAddressNormalizer::normalize(const std::string &address) const
 {
-    std::string result = address;
+    std::string result;
+    normalize(address, result);
+    return result;
+}
+
+
+void HexAddressNormalizer::operator()(const std::string &address, std::string &result) const
+{
+    return normalize(address, result);
+}
+
+
+void HexAddressNormalizer::normalize(const std::string &address, std::string &result) const
+{
+    result = address;
     if(result[1]=='x'||result[1]=='X')
     {
         result.erase(0, 2);
     }
 
     std::transform(result.begin(), result.end(), result.begin(), ::tolower);
-    return result;
 }
 
 }
--- src/detail/HexAddressNormalizer.hpp
+++ src/detail/HexAddressNormalizer.hpp
@@ -13,6 +13,8 @@ class HexAddressNormalizer
         std::string operator()(const std::string &) const;
         std::string normalize(const std::string &) const;
 
+        void operator()(const std::string &, std::string &) const;
+        void normalize(const std::string &, std::string &) const;
 };
 
 
