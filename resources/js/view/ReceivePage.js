c09ab0802bca5ae030d514c97b13af5c3a303af0
Victor Bucataru <ping@devktor.com>
2016-04-14 04:26:06 +0300

    hide stealth payment addresses

--- resources/js/view/ReceivePage.js
+++ resources/js/view/ReceivePage.js
@@ -8,7 +8,7 @@ var ReceivePageView = SubPageView.extend({
         this.template = options.templates.get("receive");
                 
         this.accounts = new AccountSelect({collection:options.accounts, templates:options.templates});
-        this.accounts.filter(function(model){return model!=undefined;}); //hide text
+        this.accounts.filter(function(model){return model!=undefined && !(model.get("address") && model.get("stealth"));}); //hide text and stealth payments
     },
     
     render:function(){
