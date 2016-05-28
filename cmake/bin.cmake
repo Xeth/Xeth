24f5a11ab3da9ef562f9945c1d85fac9c0d2a213
Victor Bucataru <ping@devktor.com>
2016-05-28 21:13:04 +0300

    detect already installed vendor app's

--- cmake/bin.cmake
+++ cmake/bin.cmake
@@ -1,46 +1,50 @@
+function(VENDOR_CHECK_DOWNLOAD EXEC PLATFORM ARCH)
+
+    find_program(${EXEC}_PATH ${EXEC})
+
+    if(${EXEC}_PATH)
+        message(${EXEC} " - found ${${EXEC}_PATH}")
+    else()
+        message(${EXEC} " - not found")
+    endif()
+    
+    if(NOT ${EXEC}_PATH AND NOT EXISTS ${PROJECT_BINARY_DIR}/vendor/bin/${EXEC})
+        set(DOWNLOAD_URL "https://sourceforge.net/projects/xeth/files/${PLATFORM}/${ARCH}/${EXEC}/download")
+        file(DOWNLOAD ${DOWNLOAD_URL} ${PROJECT_BINARY_DIR}/download/${EXEC} SHOW_PROGRESS)
+        file(COPY ${PROJECT_BINARY_DIR}/download/${EXEC} DESTINATION ${PROJECT_BINARY_DIR}/vendor/bin FILE_PERMISSIONS OWNER_READ OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE NO_SOURCE_PERMISSIONS)
+        file(REMOVE ${PROJECT_BINARY_DIR}/download/${EXEC})
+    endif()
+
+endfunction(VENDOR_CHECK_DOWNLOAD)
+
 if(NOT NO_BIN_DOWNLOAD)
 
     include(CheckTypeSize)
     check_type_size("void*" SIZEOF_VOID_P BUILTIN_TYPES_ONLY)
 
     if(SIZEOF_VOID_P EQUAL 8)
-        set(BIN_ARCH_PREFIX "x86_64")
+        set(ARCH "x86_64")
     else()
-        set(BIN_ARCH_PREFIX "x86")
+        set(ARCH "x86")
     endif()
 
 
-    set(BIN_EXTENSION "")
+    set(GETH_BIN "geth")
+    set(IPFS_BIN "ipfs")
 
     if(MSVC OR WIN32)
-        set(GETH_DOWNLOAD_URL "https://sourceforge.net/projects/xeth/files/windows/${BIN_ARCH_PREFIX}/geth.exe/download")
-        set(IPFS_DOWNLOAD_URL "https://sourceforge.net/projects/xeth/files/windows/${BIN_ARCH_PREFIX}/ipfs.exe/download")
-        set(BIN_EXTENSION ".exe")
+        set(PLATFORM "windows")
+        set(GETH_BIN "geth.exe")
+        set(IPFS_BIN "ipfs.exe")
     elseif(APPLE)
-        set(GETH_DOWNLOAD_URL "https://sourceforge.net/projects/xeth/files/osx/x86_64/geth/download")
-        set(IPFS_DOWNLOAD_URL "https://sourceforge.net/projects/xeth/files/osx/x86_64/ipfs/download")
+        set(PLATFORM "osx")
+        set(ARCH "x86_64")
     else()
-        set(GETH_DOWNLOAD_URL "https://sourceforge.net/projects/xeth/files/linux/${BIN_ARCH_PREFIX}/geth/download")
-        set(IPFS_DOWNLOAD_URL "https://sourceforge.net/projects/xeth/files/linux/${BIN_ARCH_PREFIX}/ipfs/download")
-    endif()
-
-    set(GETH_BIN "geth${BIN_EXTENSION}")
-    set(IPFS_BIN "ipfs${BIN_EXTENSION}")
-
-
-    if(NOT EXISTS ${PROJECT_BINARY_DIR}/vendor/bin/${GETH_BIN})
-        message("downloading geth")
-        file(DOWNLOAD ${GETH_DOWNLOAD_URL} ${PROJECT_BINARY_DIR}/download/${GETH_BIN} SHOW_PROGRESS)
-        file(COPY ${PROJECT_BINARY_DIR}/download/${GETH_BIN} DESTINATION ${PROJECT_BINARY_DIR}/vendor/bin FILE_PERMISSIONS OWNER_READ OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE NO_SOURCE_PERMISSIONS)
-        file(REMOVE ${PROJECT_BINARY_DIR}/download/${GETH_BIN})
+        set(PLATFORM "linux")
     endif()
 
 
-    if(NOT EXISTS ${PROJECT_BINARY_DIR}/vendor/bin/${IPFS_BIN})
-        message("downloading ipfs")
-        file(DOWNLOAD ${IPFS_DOWNLOAD_URL} ${PROJECT_BINARY_DIR}/download/${IPFS_BIN} SHOW_PROGRESS)
-        file(COPY ${PROJECT_BINARY_DIR}/download/${IPFS_BIN} DESTINATION ${PROJECT_BINARY_DIR}/vendor/bin FILE_PERMISSIONS OWNER_READ OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE NO_SOURCE_PERMISSIONS)
-        file(REMOVE ${PROJECT_BINARY_DIR}/download/${IPFS_BIN})
-    endif()
+    VENDOR_CHECK_DOWNLOAD(${GETH_BIN} ${PLATFORM} ${ARCH})
+    VENDOR_CHECK_DOWNLOAD(${IPFS_BIN} ${PLATFORM} ${ARCH})
 
 endif()
