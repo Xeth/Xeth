8315e2e5fc4d55d912fae1fda4c771ca461a9eb9
Victor Bucataru <ping@devktor.com>
2016-10-04 18:00:18 +0300

    find program hints

--- cmake/bin.cmake
+++ cmake/bin.cmake
@@ -2,10 +2,10 @@ include(${PROJECT_SOURCE_DIR}/cmake/modules/Vendor.cmake)
 
 
 if(USING_GETH)
-    REQUIRE_VENDOR_BIN("geth" "${GETH_PATH}")
+    REQUIRE_VENDOR_BIN("geth" "${GETH_PATH}" "")
 else()
-    REQUIRE_VENDOR_BIN("parity" "${PARITY_PATH}")
+    REQUIRE_VENDOR_BIN("parity" "${PARITY_PATH}" "C:\\Program Files\\Parity")
 endif()
 
 
-REQUIRE_VENDOR_BIN("ipfs" "${IPFS_PATH}")
+REQUIRE_VENDOR_BIN("ipfs" "${IPFS_PATH}" "C:\\Program Files\\Ipfs")
