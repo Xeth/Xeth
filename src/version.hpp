edb346a16714941a602688bba6f62cead2b4cd59
Victor Bucataru <ping@devktor.com>
2016-10-16 15:47:03 +0300

    version update

--- src/version.hpp
+++ src/version.hpp
@@ -3,7 +3,7 @@
 
 #define XETH_VERSION_MAJOR  0
 #define XETH_VERSION_MINTOR 3
-#define XETH_VERSION_PATCH  1
+#define XETH_VERSION_PATCH  2
 
 
-#define XETH_VERSION "0.3-1 (BETA)"
+#define XETH_VERSION "0.3-2 (BETA)"
