7fed8d79f8ee013f23f404b0e3ccede9d30d1ba0
Victor Bucataru <ping@devktor.com>
2017-03-27 22:03:49 +0300

    auto include test scripts/styles

--- cmake/resources.cmake
+++ cmake/resources.cmake
@@ -22,33 +22,49 @@ COMPILE_QRC(icon)
 
 
 
-function(INCLUDE_COMPONENT TARGET_NAME CMD DIR DEPS_FILE DEPENDENCY EXTRA)
-    add_custom_target(${TARGET_NAME} COMMAND ${PROJECT_BINARY_DIR}/editor ${EXTRA} --dependencies=${DEPS_FILE} --prefix=../ ${CMD} html/index.html ${DIR} WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/resources DEPENDS ${DEPENDENCY})
+function(INCLUDE_COMPONENT TARGET_NAME CMD HTML_FILE DIR DEPS_FILE DEPENDENCY)
+    add_custom_target(${TARGET_NAME} COMMAND ${PROJECT_BINARY_DIR}/editor --dependencies=${DEPS_FILE} --prefix=../ ${CMD} ${HTML_FILE} ${DIR} WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/resources DEPENDS ${DEPENDENCY})
 endfunction(INCLUDE_COMPONENT)
 
-function(INCLUDE_CSS TARGET_NAME DIR DEPENDENCY)
-    INCLUDE_COMPONENT(${TARGET_NAME} --include-css ${DIR} CSS/dependencies.xml ${DEPENDENCY} --body=0)
+function(INCLUDE_CSS TARGET_NAME HTML_FILE DIR DEPENDENCY)
+    INCLUDE_COMPONENT(${TARGET_NAME} --include-css ${HTML_FILE} ${DIR} CSS/dependencies.xml ${DEPENDENCY} )
 endfunction(INCLUDE_CSS)
 
-function(INCLUDE_JS TARGET_NAME DIR DEPENDENCY)
-    INCLUDE_COMPONENT(${TARGET_NAME} --include-js ${DIR} js/dependencies.xml ${DEPENDENCY} --body=0)
+function(INCLUDE_JS TARGET_NAME HTML_FILE DIR DEPENDENCY)
+    INCLUDE_COMPONENT(${TARGET_NAME} --include-js ${HTML_FILE} ${DIR} js/dependencies.xml ${DEPENDENCY})
 endfunction(INCLUDE_JS)
 
-INCLUDE_CSS(css_vendor CSS/vendor ${PROJECT_BINARY_DIR}/editor)
-INCLUDE_CSS(css_core CSS/core css_vendor)
-INCLUDE_CSS(css_style CSS/style css_core)
+function(INCLUDE_SCRIPTS TARGET HTML_FILE)
+    INCLUDE_JS(${TARGET}_js_vendor_core ${HTML_FILE} js/vendor/core ${PROJECT_BINARY_DIR}/editor)
+    INCLUDE_JS(${TARGET}_js_vendor_libs ${HTML_FILE} js/vendor/libs ${TARGET}_js_vendor_core)
+    INCLUDE_JS(${TARGET}_js_core js/core ${HTML_FILE} ${TARGET}_js_vendor_libs)
+    INCLUDE_JS(${TARGET}_js_models ${HTML_FILE} js/model ${TARGET}_js_core)
+    INCLUDE_JS(${TARGET}_js_views ${HTML_FILE} js/view ${TARGET}_js_models)
+    add_custom_target(${TARGET} DEPENDS ${TARGET}_js_views)
+endfunction(INCLUDE_SCRIPTS)
 
-INCLUDE_JS(js_vendor_core js/vendor/core ${PROJECT_BINARY_DIR}/editor)
-INCLUDE_JS(js_vendor_libs js/vendor/libs js_vendor_core)
-INCLUDE_JS(js_core js/core js_vendor_libs)
-INCLUDE_JS(js_models js/model js_core)
-INCLUDE_JS(js_views js/view js_models)
+function(INCLUDE_STYLES TARGET HTML_FILE)
+    INCLUDE_CSS(${TARGET}_css_vendor ${HTML_FILE} CSS/vendor ${PROJECT_BINARY_DIR}/editor)
+    INCLUDE_CSS(${TARGET}_css_core ${HTML_FILE} CSS/core ${TARGET}_css_vendor)
+    INCLUDE_CSS(${TARGET}_css_style ${HTML_FILE} CSS/style ${TARGET}_css_core)
+    add_custom_target(${TARGET} DEPENDS ${TARGET}_css_style)
+endfunction(INCLUDE_STYLES)
+
+
+INCLUDE_SCRIPTS(include_scripts html/index.html)
+INCLUDE_STYLES(include_styles html/index.html)
+
+INCLUDE_SCRIPTS(test_base_scripts test/test.html)
+INCLUDE_JS(test_scripts test/test.html simulator test_base_scripts)
+INCLUDE_STYLES(test_styles test/test.html)
+
+add_custom_target(test_html DEPENDS test_styles test_scripts)
 
 
 COMPILE_QRC(html)
 
-add_dependencies(compile_html css_style)
-add_dependencies(compile_html js_views)
+add_dependencies(compile_html include_styles)
+add_dependencies(compile_html include_scripts)
 
 
 
