082258f906db56e9a2e69dcb9e000e9152589568
Victor Bucataru <ping@devktor.com>
2017-04-06 22:37:15 +0300

    fixed account scan criterion test

--- test/case/synchronizer/AccountScanCriterionTest.cpp
+++ test/case/synchronizer/AccountScanCriterionTest.cpp
@@ -2,21 +2,21 @@
 
 
 AccountScanCriterionTest::AccountScanCriterionTest() :
-    _criterion("testaddress")
+    _criterion("0xfffff")
 {}
 
 void AccountScanCriterionTest::testMinedTransaction()
 {
     Xeth::ScanResult result;
 
-    _criterion.processHeader(1, "blockhash", "testaddress", Xeth::BigInt("10000"), 1000, result);
+    _criterion.processHeader(1, "blockhash", "0xfffff", Xeth::BigInt("10000"), 1000, result);
 
     QVERIFY(result.transactions.size()==1);
 
     QJsonObject transaction = result.transactions.begin()->toObject();
 
 
-    QVERIFY(transaction["to"].toString() == "testaddress");
+    QVERIFY(transaction["to"].toString() == "0xfffff");
     QVERIFY(transaction["from"].isNull());
     QVERIFY(transaction["amount"].toString() == boost::lexical_cast<std::string>(Xeth::BigInt("10000")).c_str());
     QVERIFY(transaction["category"].toString() == Xeth::TransactionCategory::ToString(Xeth::TransactionCategory::Mined));
@@ -27,15 +27,15 @@ void AccountScanCriterionTest::testReceivedTransaction()
 {
     Xeth::ScanResult result;
 
-    _criterion.processTransaction("somehash", "senderaddress", "testaddress", Xeth::BigInt(10001), "", 1001, result);
+    _criterion.processTransaction("somehash", "0xfffff1", "0xfffff", Xeth::BigInt(10001), "", 1001, result);
 
     QVERIFY(result.transactions.size()==1);
 
     QJsonObject transaction = result.transactions.begin()->toObject();
 
 
-    QVERIFY(transaction["to"].toString() == "testaddress");
-    QVERIFY(transaction["from"].toString() == "senderaddress");
+    QVERIFY(transaction["to"].toString() == "0xfffff");
+    QVERIFY(transaction["from"].toString() == "0xfffff1");
     QVERIFY(transaction["amount"].toString() == boost::lexical_cast<std::string>(Xeth::BigInt(10001)).c_str());
     QVERIFY(transaction["category"].toString() == Xeth::TransactionCategory::ToString(Xeth::TransactionCategory::Received));
     QVERIFY(transaction["timestamp"].toInt() == 1001);
@@ -45,15 +45,15 @@ void AccountScanCriterionTest::testSentTransaction()
 {
     Xeth::ScanResult result;
 
-    _criterion.processTransaction("somehash", "testaddress", "receiveraddress", Xeth::BigInt(10002), "", 1002, result);
+    _criterion.processTransaction("somehash", "0xfffff", "0xfffff1", Xeth::BigInt(10002), "", 1002, result);
 
     QVERIFY(result.transactions.size()==1);
 
     QJsonObject transaction = result.transactions.begin()->toObject();
 
 
-    QVERIFY(transaction["to"].toString() == "receiveraddress");
-    QVERIFY(transaction["from"].toString() == "testaddress");
+    QVERIFY(transaction["to"].toString() == "0xfffff1");
+    QVERIFY(transaction["from"].toString() == "0xfffff");
     QVERIFY(transaction["amount"].toString() == boost::lexical_cast<std::string>(Xeth::BigInt(10002)).c_str());
     QVERIFY(transaction["category"].toString() == Xeth::TransactionCategory::ToString(Xeth::TransactionCategory::Sent));
     QVERIFY(transaction["timestamp"].toInt() == 1002);
@@ -63,7 +63,7 @@ void AccountScanCriterionTest::testUnknownTransaction()
 {
     Xeth::ScanResult result;
 
-    _criterion.processTransaction("somehash", "senderaddress", "receiveraddress", Xeth::BigInt(10003), "", 1003, result);
+    _criterion.processTransaction("somehash", "0xfffff1", "0xfffff2", Xeth::BigInt(10003), "", 1003, result);
 
     QVERIFY(result.transactions.size()==0);
 }
