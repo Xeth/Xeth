ce403920e5d8084355d8b96abf67a43018bd0da4
Victor Bucataru <ping@devktor.com>
2017-04-06 22:28:33 +0300

    fixed convertor unit tests

--- test/case/converter/FinneyConversionTest.cpp
+++ test/case/converter/FinneyConversionTest.cpp
@@ -7,11 +7,11 @@ void FinneyConversionTest::testFromFinney()
     QVariant request = QString("1000");
     QVariant result = command(request);
 
-    QCOMPARE(result.toString(), QString("1"));
+    QCOMPARE(result.toString(), QString("1.00000000000000000000"));
 
     request = QString("10000000000000000000");
     result = command(request);
-    QCOMPARE(result.toString(), QString("10000000000000000"));
+    QCOMPARE(result.toString(), QString("10000000000000000.00000000000000000000"));
 
 }
 
